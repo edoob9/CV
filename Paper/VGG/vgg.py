f60bb41d226b9de3a1174c1386231b29e6832c41
이시우 <47210353+edenLee94@users.noreply.github.com>
2022-06-15 03:30:10 +0900

    Create vgg.py
    논문의 구조에 따라서 설정하면 이런 형태의 구조가 나올 것 같다. 근데, tensorflow에서 제공하는 document를 읽다보니까 그냥 api를 바로 사용해도 되는 것으로 볼 수 있다.
    -> 해당 내용에 대해서 학습할 필요가 있다. 학습은 kaggle data에서 dog-and-cat 구별로 적용해봐야겠다!

---
+++ Paper/VGG/vgg.py
@@ -0,0 +1,43 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
+model = Sequential()
+model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=2, activation="softmax"))
+
+## VGG model + classfication
+
+model = Sequential()
+model.add(VGG)
+model.add(Flatten())
+model.add(Dense(units=256,activation="relu"))
+model.add(Dense(units=256,activation="relu"))
+model.add(Dense(units=2, activation="softmax"))
+
